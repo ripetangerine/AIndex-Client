10812313cb0c79fa7638ad09cba481a92fc6455d
cuzurmyhabit <s2472@e-mirim.hs.kr>
2025-06-17 04:33:04 +0900

    feat: 검색 기능

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import Stars from './pages/Stars'; 
 import AddAI from './pages/AddAI'; 
 import Chat from './pages/Chat';   
+import Search from './pages/Search';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path="/stars" element={<Stars />} />
       <Route path="/add-ai" element={<AddAI />} />
       <Route path="/chat" element={<Chat />} />
+      <Route path="/search" element={<Search />} />
       {/* 다른 AI 상세 페이지 라우트도 여기에 추가 */}
       <Route path="*" element={<div><h1>404 페이지를 찾을 수 없습니다</h1></div>} />
     </Routes>
